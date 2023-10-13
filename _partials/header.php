026ad82af32e2c8f82926208a79a14cbf12925da
radimir007 <radovan.psotny123@gmail.com>
2023-10-13 18:06:48 +0200

    Created header component + style for it; smaller progress

---
+++ _partials/header.php
@@ -0,0 +1,6 @@
+<nav>
+    <div class="logo">
+        <img src="assets/img/logo_transparent_black.png" alt="">
+    </div>
+    <a href="#">Log In</a>
+</nav>
--- index.php
+++ index.php
@@ -6,8 +6,12 @@
           content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Vocabulary trainer</title>
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/header.css">
 </head>
 <body>
+    <?php include_once "_partials/header.php"?>
     <h1>Hello world!</h1>
 </body>
 </html>
