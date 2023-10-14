6f47c4302782e588ff41349dcdac7d63ae37fb42
radimir007 <radovan.psotny123@gmail.com>
2023-10-14 19:26:38 +0200

    Modelling main page

--- _partials/header.php
+++ _partials/header.php
@@ -1,6 +1,10 @@
 <nav>
     <div class="logo">
-        <a href="index.php"><img src="assets/img/logo.png" alt=""></a>
+        <a href="index.php">
+            <img src="assets/img/lightbulb.webp" alt="">
+            <span>Vocabulary trainer</span>
+        </a>
     </div>
-    <a href="#"><i class="fa-regular fa-circle-user"></i></a>
+    <a href="#">Log in<i class="fa-regular fa-circle-user" style="color: black;
+    margin-left: .6rem; font-size: 1.8rem;"></i></a>
 </nav>
--- index.php
+++ index.php
@@ -7,12 +7,31 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Vocabulary trainer</title>
     <link rel="stylesheet" href="assets/css/normalize.css">
-    <link rel="stylesheet" href="assets/font_awesome/css/">
+    <link rel="stylesheet" href="assets/font_awesome/css/all.min.css">
     <link rel="stylesheet" href="assets/css/main.css">
     <link rel="stylesheet" href="assets/css/header.css">
 </head>
 <body>
     <?php include_once "_partials/header.php"?>
-    <h1>Hello world!</h1>
+    <main>
+        <h1>Subjects</h1>
+        <section class="subject-list">
+            <article>
+                <img src="assets/img/german.jpg" alt="">
+                <div class="subject-description">
+                    <h2>German</h2>
+                    <small>14 thematic units</small>
+                </div>
+            </article>
+            <article>
+                <img src="assets/img/british.jpg" alt="">
+                <div class="subject-description">
+                    <h2>English</h2>
+                    <small>11 thematic units</small>
+                </div>
+
+            </article>
+        </section>
+    </main>
 </body>
 </html>
