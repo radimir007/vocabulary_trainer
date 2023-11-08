62808747ddb37a408d7ba9379506869759a450e2
radimir007 <radovan.psotny123@gmail.com>
2023-11-08 18:33:04 +0100

    Minor improvements in styling

--- study.php
+++ study.php
@@ -16,12 +16,27 @@
           content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= get_th_unit_name($unit_id)[0] ?></title>
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/study.css">
 </head>
 <body>
 
-    <?php foreach (get_words($unit_id) as $term): ?>
+    <?php include_once "_partials/header.php"?>
 
-    <?php endforeach; ?>
+    <main>
+        <ul>
+            <?php foreach (get_words($unit_id) as $term): ?>
+                <li>
+                    <span><?= $term[0] ?></span>
+                    <span><?= $term[1] ?></span>
+                </li>
+
+
+            <?php endforeach; ?>
+        </ul>
+    </main>
+
 
 </body>
 </html>
