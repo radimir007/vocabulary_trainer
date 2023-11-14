d7c7a7dac0da86955ad965bdedf1df22e63d9876
radimir007 <radovan.psotny123@gmail.com>
2023-11-14 12:35:52 +0100

    Created flashcards section

---
+++ flashcards.php
@@ -0,0 +1,21 @@
+<?php
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/flashcards.css">
+</head>
+<body>
+
+    <?php include_once "_partials/header.php" ?>
+
+</body>
+</html>
